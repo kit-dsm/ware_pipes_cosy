a2332ce09870548a6377516a1cc5dbe62aac239e
j4n1k <janik.bischoff@gmx.de>
2026-07-08 10:53:10 +0200

    revert ibrsp run script change

--- experiments/run_ibrsp.py
+++ experiments/run_ibrsp.py
@@ -22,7 +22,7 @@ class IBRSPRunner(PipelineRunner):
         for filepath in self.instances_dir.glob("*.txt"):
             if filepath.is_file():
                 instances.append((filepath.stem, [filepath]))
-                if len(instances) == 1:
+                if len(instances) == 100:
                     break
         return instances
 
